3578fd7be100b7d657100e788dbc94de109382ae
Boris Serebrov <serebrov@gmail.com>
2018-05-01 11:33:36 +0300

    Move getOrgCACerts to common script.

--- main-network-setup/scripts/env.sh
+++ main-network-setup/scripts/env.sh
@@ -44,6 +44,46 @@ function finishMSPSetup {
 }
 
 
+# Get Organization CA certificates
+function getOrgCACerts {
+   ORG=$ORDERER_ORG
+   ORG_MSP_DIR=/data/fabric.opetbot.com/msp
+   ORG_ADMIN_HOME=/data/fabric.opetbot.com/admin
+   ORG_ADMIN_CERT=${ORG_MSP_DIR}/admincerts/cert.pem
+   ORG_ADMIN_HOME=/data/fabric.opetbot.com/admin
+
+   log "Getting CA certs for organization $ORG and storing in $ORG_MSP_DIR"
+   export FABRIC_CA_CLIENT_TLS_CERTFILES=$CA_CERTFILE
+
+   fabric-ca-client getcacert -d -u https://$CA_HOST:7054 -M $ORG_MSP_DIR
+   finishMSPSetup $ORG_MSP_DIR
+
+   # switchToAdminIdentity
+   export FABRIC_CA_CLIENT_HOME=$ORG_ADMIN_HOME
+   # export FABRIC_CA_CLIENT_TLS_CERTFILES=$CA_CERTFILE
+   export FABRIC_CA_CLIENT_TLS_CERTFILES=$ORG_MSP_DIR/tlscacerts/ca-fabric-opetbot-com-7054.pem
+   fabric-ca-client enroll -d -u https://$ORG_ADMIN_USER:$ORG_ADMIN_PASS@$CA_HOST:7054
+   # If admincerts are required in the MSP, copy the cert there now and to my local MSP also
+   mkdir -p $(dirname "${ORG_ADMIN_CERT}")
+   cp $ORG_ADMIN_HOME/msp/signcerts/* $ORG_ADMIN_CERT
+   mkdir $ORG_ADMIN_HOME/msp/admincerts
+   cp $ORG_ADMIN_HOME/msp/signcerts/* $ORG_ADMIN_HOME/msp/admincerts
+}
+
+
+# Copy the org's admin cert into some target MSP directory
+# This is only required if ADMINCERTS is enabled.
+# To generate the admin certificates, call getOrgCACerts first.
+function copyAdminCert {
+   if [ $# -ne 1 ]; then
+      fatal "Usage: copyAdminCert <targetMSPDIR>"
+   fi
+   dstDir=$1/admincerts
+   mkdir -p $dstDir
+   cp $ORG_ADMIN_CERT $dstDir
+}
+
+
 function generateChannelArtifacts() {
   ORG=$ORDERER_ORG
 
--- main-network-setup/scripts/register-ca-identities.sh
+++ main-network-setup/scripts/register-ca-identities.sh
@@ -30,7 +30,7 @@ function registerOrdererIdentities {
 function registerOrganizationAdmin {
    log "Registering organization admin identity with $CA_NAME"
    # The admin identity has the "admin" attribute which is added to ECert by default
-   fabric-ca-client register -d --id.name $ADMIN_NAME --id.secret $ADMIN_PASS --id.attrs "admin=true:ecert"
+   fabric-ca-client register -d --id.name $ORG_ADMIN_USER --id.secret $ORG_ADMIN_PASS --id.attrs "admin=true:ecert"
 
    # The peer organization admin
    # fabric-ca-client register -d --id.name $ADMIN_NAME --id.secret $ADMIN_PASS --id.attrs "hf.Registrar.Roles=client,hf.Registrar.Attributes=*,hf.Revoker=true,hf.GenCRL=true,admin=true:ecert,abac.init=true:ecert"
@@ -47,35 +47,6 @@ function registerPeerIdentities {
    PEER_PASS=$PEER0_PASS
    log "Registering $PEER_NAME with $CA_NAME"
    fabric-ca-client register -d --id.name $PEER_NAME --id.secret $PEER_PASS --id.type peer
-}
-
-
-# Get Organization CA certificates
-function getOrgCACerts {
-   ORG=$ORDERER_ORG
-   ORG_MSP_DIR=/data/fabric.opetbot.com/msp
-   ORG_ADMIN_HOME=/data/fabric.opetbot.com/admin
-   ORG_ADMIN_CERT=${ORG_MSP_DIR}/admincerts/cert.pem
-   ORG_ADMIN_HOME=/data/fabric.opetbot.com/admin
-
-   log "Getting CA certs for organization $ORG and storing in $ORG_MSP_DIR"
-   export FABRIC_CA_CLIENT_TLS_CERTFILES=$CA_CERTFILE
-
-   fabric-ca-client getcacert -d -u https://$CA_HOST:7054 -M $ORG_MSP_DIR
-   finishMSPSetup $ORG_MSP_DIR
-   # If ADMINCERTS is true, we need to enroll the admin now to populate the admincerts directory
-   if [ $ADMINCERTS ]; then
-      # switchToAdminIdentity
-      export FABRIC_CA_CLIENT_HOME=$ORG_ADMIN_HOME
-      # export FABRIC_CA_CLIENT_TLS_CERTFILES=$CA_CERTFILE
-      export FABRIC_CA_CLIENT_TLS_CERTFILES=$ORG_MSP_DIR/tlsca/tlsca.fabric.opetbot.com-cert.pem
-      fabric-ca-client enroll -d -u https://$ORG_ADMIN_USER:$ORG_ADMIN_PASS@$CA_HOST:7054
-      # If admincerts are required in the MSP, copy the cert there now and to my local MSP also
-      mkdir -p $(dirname "${ORG_ADMIN_CERT}")
-      cp $ORG_ADMIN_HOME/msp/signcerts/* $ORG_ADMIN_CERT
-      mkdir $ORG_ADMIN_HOME/msp/admincerts
-      cp $ORG_ADMIN_HOME/msp/signcerts/* $ORG_ADMIN_HOME/msp/admincerts
-   fi
 }
 
 
